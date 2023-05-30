543880b3d767d0ca9b2f7f34501a364e23e9295d
EC2 Default User <ec2-user@ip-172-31-6-163.ap-northeast-1.compute.internal>
2023-05-30 12:38:09 +0000

    [Star]11

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,22 +3,24 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new
   end
   
+  # 投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_image_path
+    redirect_to post_images_path
   end
-  
   def index
+    @post_images = PostImage.all
   end
 
   def show
   end
   
+  # 投稿データのストロングパラメータ
   private
-  
-  def post_image_paramas
-    paramas.require(:post_image).permit(:shop_name, :image, :caption)
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
